322868f72510803f7c482cfc07d0b0c71d4a2bf9
RaedGhodhbane <ghodhbane.raed@gmail.com>
2021-11-18 21:39:54 +0100

    Fix bodyParser deprecated

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
     });
-app.use(bodyParser.json())
+app.use(express.json())
 
 app.get('/', function(req,res){
     res.send('hello word')
